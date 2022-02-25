f1f16089cbbaf09e6eb6f5e0c615f6076f5e1a8c
niklas7U <20099542@mail.wit.ie>
2022-02-25 14:21:53 +0000

    Initial Commit
    The notes-app-v1 Project of the assessment 2.1 in SDT, WIT

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.ronklas"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    /// Own dependencies:
+
+    // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    /*testI*/implementation("org.slf4j:slf4j-simple:2.0.0-alpha6")
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
