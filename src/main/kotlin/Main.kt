5f0031bd714540614e8658fc3b35e0485a8c3489
niklas7U <20099542@mail.wit.ie>
2022-03-04 14:15:36 +0000

    INFO Skeleton menu for Notes written mainly covered with last commit. Changed visual appearance of the Menu for the user a bit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,19 @@
-
-
 // Qestion agile
 val scanner = java.util.Scanner(System.`in`)
 
 fun showMenu() : Int {
     print("""
-        > -------------------
-        > NOTE KEEPER APP
-        > -------------------
-        > NOTE MENU
-        >    1) Add a note
-        >    2) List all notes
-        >    3) Update a note
-        >    4) Delete a note
-        > -------------------
-        >    0) Exit
-        > -------------------
+        > ---------------------------
+        > |  NOTE KEEPER APP        |
+        > ---------------------------
+        > | NOTE MENU               |
+        > |   1) Add a note         |
+        > |   2) List all notes     |
+        > |   3) Update a note      |
+        > |   4) Delete a note      |
+        > ---------------------------
+        > |   0) Exit               |
+        > ---------------------------
         > ==>> 
     """.trimMargin(">"))
     return scanner.nextInt()
