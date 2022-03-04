0fd022522952722e97f8e9a6159b1212525ec542
niklas7U <20099542@mail.wit.ie>
2022-03-04 14:08:51 +0000

    FEAT viusal Menu creating a visual Menu with functions showMenu, runMenu adapting main too therefore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+// Qestion agile
+val scanner = java.util.Scanner(System.`in`)
+
+fun showMenu() : Int {
+    print("""
+        > -------------------
+        > NOTE KEEPER APP
+        > -------------------
+        > NOTE MENU
+        >    1) Add a note
+        >    2) List all notes
+        >    3) Update a note
+        >    4) Delete a note
+        > -------------------
+        >    0) Exit
+        > -------------------
+        > ==>> 
+    """.trimMargin(">"))
+    return scanner.nextInt()
+}
+
+fun runMenu() {
+    do {
+        when (val menu = showMenu()) {
+            1 -> addNote()
+            2 -> readNote()
+            3 -> updateNote()
+            4 -> deleteNote()
+            0 -> exitProgram()
+            else -> println("Option $menu is invalid. Try another one")
+        }
+    } while (true)
+}
+
+fun exitProgram() {
+    println("bye, see you :)")
+    kotlin.system.exitProcess(0)
+}
+
+fun deleteNote() {
+    //TODO("Not yet implemented")
+}
+
+fun updateNote() {
+    //TODO("Not yet implemented")
+}
+
+fun readNote() {
+    //TODO("Not yet implemented")
+}
+
+fun addNote() {
+    //TODO("Not yet implemented")
+}
+
+fun main() {
+    runMenu()
 }
