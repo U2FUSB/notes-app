612c327ec22dc96abb8c66d0abb0404925b2bf2e
niklas7U <20099542@mail.wit.ie>
2022-03-12 19:50:52 +0000

    IMPL updating of notes
    You can now update notes, by providing their corresponding index. (Does not support change of archived-status yet)
    
    Also, JUnit Tests are provided for corresponding functions.

--- src/main/kotlin/controller/NoteAPI.kt
+++ src/main/kotlin/controller/NoteAPI.kt
@@ -1,5 +1,5 @@
 package controller
-//
+
 import models.Note
 
 class NoteAPI {
@@ -57,6 +57,21 @@ class NoteAPI {
             notes.removeAt(indexToDelete)
         } else null
     }
+    fun updateNote(indexToUpdate: Int, note: Note?): Boolean {
+        //find the note object by the index number
+        val foundNote = findNote(indexToUpdate)
+
+        //if the note exists, use the note details passed as parameters to update the found note in the ArrayList.
+        if ((foundNote != null) && (note != null)) {
+            foundNote.noteTitle = note.noteTitle
+            foundNote.notePriority = note.notePriority
+            foundNote.noteCategory = note.noteCategory
+            return true
+        }
+
+        //if the note was not found, return false, indicating that the update was not successful
+        return false
+    }
     fun numberOfNotes(): Int {
         return notes.size
     }
@@ -68,6 +83,9 @@ class NoteAPI {
     }
     fun numberOfNotesByPriority(priority: Int): Int {
         return notes.count{it.notePriority == priority}
+    }
+    fun isValidIndex(index: Int) :Boolean{
+        return isValidListIndex(index, notes);
     }
     private fun isValidListIndex(index: Int, list: List<Any>):Boolean {
         return (index >= 0 && index < list.size)
